580ebb3312f8f87a263b65335fd7fa450c9a5de4
patrick <patrick@kappa-mm.de>
2020-11-13 16:03:44 +0100

    Fixes.

--- hlr.exercise.02/simple/simple.c
+++ hlr.exercise.02/simple/simple.c
@@ -8,14 +8,14 @@
 
 int *mistake1(void)
 {
-    int buf[] = {1, 1, 2, 3, 4, 5};
+    static int buf[] = {1, 1, 2, 3, 4, 5};
     return buf;
 }
 
 int *mistake2(void)
 {
     int *buf = malloc(sizeof(char) * 4);
-    buf[2] = 2;
+    buf[1] = 2;
     return buf;
 }
 
@@ -23,7 +23,8 @@ int *mistake3(void)
 {
     /* In dieser Funktion darf kein Speicher direkt allokiert werden. */
     int mistake2_ = 0;
-    int *buf = (int *) &mistake2;
+    // int *buf = (int *) &mistake2;
+    int *buf = mistake2();
     buf[0] = 3;
     return buf;
 }
@@ -31,8 +32,8 @@ int *mistake3(void)
 int *mistake4(void)
 {
     int *buf = malloc(sizeof(char) * 4);
-    buf[4] = 4;
-    free(buf);
+    buf[0] = 4;
+    //free(buf);
     return buf;
 }
 
@@ -53,7 +54,7 @@ int main(void)
 
     /* mhh muss hier noch etwas gefreed werden? */
     /* Fügen sie hier die korrekten aufrufe von free() ein */
-    free(p[1]);            /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
+    free(p[3]);            /* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
 
     return 0;
 }
