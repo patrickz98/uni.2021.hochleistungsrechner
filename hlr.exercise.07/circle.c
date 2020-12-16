06d7c89fb5324dc7bb52658d794dc1ae2a4dd662
Patrick Zierahn <patrick@kappa-mm.de>
2020-12-16 13:29:20 +0100

    Check in.

--- hlr.exercise.07/circle.c
+++ hlr.exercise.07/circle.c
@@ -27,16 +27,21 @@ void circle(int num)
     int process;
     MPI_Comm_rank(MPI_COMM_WORLD, &process);
     
-    // We set are static buffer size across all process
+    //
+    // Set a static buffer size across all process
+    //
+
     int bufferSize = (num / processes);
     int overflow   = (num % processes);
 
     // Check if number can be split evenly across all processes
+    // if not increase buffer size
     if (overflow != 0)
     {
         bufferSize++;
     }
 
+    // Calculate how many random numbers need to be generated
     int numberCount = (num / processes);
     if (process < overflow)
     {
@@ -48,22 +53,23 @@ void circle(int num)
     // Init "random" with seed
     srand(28051998 * process);
 
+    // Fill numbers array with random numbers
+    // The -1 signals overflow
     for (int inx = 0; inx < bufferSize; inx++)
     {
-        // Fill numbers array with random numbers
-        // The -1 signals overflow
         numbers[ inx ] = (inx < numberCount)
             ? rand() % 25
             : -1;
     }
 
+    // Print out initial array 
     for (int inx = 0; inx < numberCount; inx++)
     {
-        printf("[ %d ] --> numbers[ %d ] = %d\n", process, inx, numbers[ inx ]);
+        printf("[ %d ] --> init: numbers[ %d ] = %d\n", process, inx, numbers[ inx ]);
     }
 
     //
-    // MPI Stuff
+    // Send stop nummber from P0 to last process
     //
 
     if (process == 0)
@@ -79,9 +85,9 @@ void circle(int num)
 
     int stopNumber = -1;
     int brodcastRoot = (processes - 1);
-    int isProc0 = (process == brodcastRoot);
+    int isLastProc = (process == brodcastRoot);
 
-    if (isProc0)
+    if (isLastProc)
     {
         //
         // Receive stop nummber from process[0]
@@ -91,25 +97,35 @@ void circle(int num)
         printf("[ %d ] --> Recv: %d\n", process, stopNumber);
     }
 
+    // Calculate source and destination process number for message exchange
     int source      = (process + processes - 1) % processes;
     int destination = (process + processes + 1) % processes;
 
     // printf("[ %d ] --> source: %d, destination: %d\n", process, source, destination);
 
+    //
+    // Send and receive number array
+    //
+
     int exit = 0;
-    
     while (exit == 0)
     {
         MPI_Send((void*) &numbers, bufferSize, MPI_INT, destination, TAG_SEND_NUMBERS, MPI_COMM_WORLD);
         MPI_Recv(        &numbers, bufferSize, MPI_INT,      source, TAG_SEND_NUMBERS, MPI_COMM_WORLD, &status);
 
-        if (isProc0)
+        if (isLastProc)
         {
+            // Check if number matches number from first process
             exit = (stopNumber == numbers[0]);
         }
 
+        // Brodcast termination
         MPI_Bcast((void*) &exit, 1, MPI_INT, brodcastRoot, MPI_COMM_WORLD);
     }
+
+    //
+    // Print final array after exchange
+    //
 
     for (int inx = 0; inx < bufferSize; inx++)
     {
@@ -119,6 +135,10 @@ void circle(int num)
             printf("[ %d ] --> final: numbers[ %d ] = %d\n", process, inx, number);
         }
     }
+
+    //
+    // MPI clean-up
+    //
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
